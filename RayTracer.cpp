3a1f65304f96a0874a9c55b88436f45141f5012b
Oliver Cranshaw <ojc31@uclive.ac.nz>
2021-06-01 22:32:16 +1200

    Stuff

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -28,11 +28,11 @@ const float XMAX =  WIDTH * 0.5;
 const float YMIN = -HEIGHT * 0.5;
 const float YMAX =  HEIGHT * 0.5;
 
-const float FOG_START = 0;
+const float FOG_START = -40;
 const float FOG_END = -220;
 
-TextureBMP texture;
-TextureBMP desertTexure;
+TextureBMP backgroundTexture;
+TextureBMP sateliteTexture;
 
 vector<SceneObject*> sceneObjects;
 
@@ -44,7 +44,7 @@ vector<SceneObject*> sceneObjects;
 glm::vec3 trace(Ray ray, int step)
 {
     glm::vec3 backgroundCol(0);                     //Background colour = (0,0,0)
-    glm::vec3 lightPos(10, 40, -3);                 //Light's position
+    glm::vec3 lightPos(10, 100, -3);                 //Light's position
     glm::vec3 color(0);
     SceneObject* obj;
 
@@ -67,26 +67,30 @@ glm::vec3 trace(Ray ray, int step)
         else color = glm::vec3(1, 1, 1);
         obj->setColor(color);
 
-        //Texture on ground
-        float texcoords = (ray.hit.x - -15.0)/(5.0 - -15.0);
-        float texcoordt = (ray.hit.z - -60.0)/(-90 - -60.0);
-        if(texcoords > 0 && texcoords < 1 &&
-        texcoordt > 0 && texcoordt < 1)
-        {
-            color=texture.getColorAt(texcoords, texcoordt);
-            obj->setColor(color);
-        }
     }
 
     if (ray.index == 1) {
 
-        //backWall Desert image
+        //backWall mountain image
         float texcoords = (ray.hit.x - -80.0)/(80.0 - -80.0);
         float texcoordt = (ray.hit.y - -15.0)/(60 - -15.0);
         if(texcoords > 0 && texcoords < 1 &&
         texcoordt > 0 && texcoordt < 1)
         {
-            color=desertTexure.getColorAt(texcoords, texcoordt);
+            color=backgroundTexture.getColorAt(texcoords, texcoordt);
+            obj->setColor(color);
+        }
+    }
+
+    if (ray.index == 2) {
+
+        //satelite image
+        float texcoords = (ray.hit.x - -20.0)/(-10.0 - -20.0);
+        float texcoordt = (ray.hit.y - 20.0)/(30 - 20.0);
+        if(texcoords > 0 && texcoords < 1 &&
+        texcoordt > 0 && texcoordt < 1)
+        {
+            color=sateliteTexture.getColorAt(texcoords, texcoordt);
             obj->setColor(color);
         }
     }
@@ -100,11 +104,19 @@ glm::vec3 trace(Ray ray, int step)
     Ray shadowRay(ray.hit, lightVec);
     shadowRay.closestPt(sceneObjects);
 
-
     const float lightDist = glm::length(lightVec);
 
     if (shadowRay.index > -1 && shadowRay.dist < lightDist) {
-        color = 0.2f * obj->getColor();
+
+        if (sceneObjects[shadowRay.index]->isTransparent() && sceneObjects[shadowRay.index] != obj) {
+            color = 0.6f * obj->getColor() + 0.2f * sceneObjects[shadowRay.index]->getColor();
+        } else if (sceneObjects[shadowRay.index]->isRefractive() && sceneObjects[shadowRay.index] != obj) {
+            color = 0.6f * obj->getColor() + 0.2f * sceneObjects[shadowRay.index]->getColor();
+        } else {
+            color = 0.2f * obj->getColor();
+        }
+
+
     }
 
     if (obj->isReflective() && step < MAX_STEPS)
@@ -117,7 +129,32 @@ glm::vec3 trace(Ray ray, int step)
         color = color + (rho * reflectedColor);
     }
 
-    //Fog
+    if (obj->isTransparent() && step < MAX_STEPS) {
+        float tho = obj->getTransparencyCoeff();
+        Ray transparentRay(ray.hit, ray.dir);
+        glm::vec3 transparentColor = trace(transparentRay, step + 1);
+        color = color * tho + transparentColor * (1-tho);
+    }
+
+    if (obj -> isRefractive() && step < MAX_STEPS)
+    {
+        float refr_index = obj->getRefractiveIndex();
+        float eta = 1/refr_index;
+        glm::vec3 norm = obj->normal(ray.hit);
+        glm::vec3 g = glm::refract(ray.dir, norm, eta);
+        Ray refrRay(ray.hit, g);
+        refrRay.closestPt(sceneObjects);
+        glm::vec3 m = obj->normal(refrRay.hit);
+        glm::vec3 h = glm::refract(g, -m, 1.0f / eta);
+
+        Ray refrRay2(refrRay.hit, h);
+        refrRay2.closestPt(sceneObjects);
+        glm::vec3 refrColor = trace(refrRay2, step + 1);
+
+        color = color * 0.5f + (refrColor);
+    }
+
+
     double fog_factor;
 
     if (ray.hit.z > FOG_START) {
@@ -129,7 +166,8 @@ glm::vec3 trace(Ray ray, int step)
     }
     color.x = color.x * (1 - fog_factor) + fog_factor;
     color.y = color.y * (1 - fog_factor) + fog_factor;
-//    color.z = color.z * (1 - fog_factor) + fog_factor;
+    color.z = color.z * (1 - fog_factor) + fog_factor;
+
     return color;
 }
 
@@ -189,8 +227,8 @@ void initialize()
 
     glClearColor(0, 0, 0, 1);
 
-    texture = TextureBMP("Butterfly.bmp");
-    desertTexure = TextureBMP("Desert.bmp");
+    backgroundTexture = TextureBMP("background.bmp");
+    sateliteTexture = TextureBMP("satelite.bmp");
 
 
     Plane *plane = new Plane (glm::vec3(-80., -15, -40), //Point A
@@ -208,25 +246,28 @@ void initialize()
     backWall->setSpecularity(false);
     sceneObjects.push_back(backWall);
 
-    Sphere *sphere1 = new Sphere(glm::vec3(-5.0, 0.0, -150.0), 15.0);
+
+    Sphere *sphere4 = new Sphere(glm::vec3(-15.0, 25.0, -130.0), 5.0);
+    sceneObjects.push_back(sphere4);         //Add sphere to scene objects
+
+    Sphere *sphere1 = new Sphere(glm::vec3(15.0, 5.0, -100.0), 9.0);
     sphere1->setColor(glm::vec3(0, 0, 1));
+    sphere1->setRefractivity(true, 0.5, 1.07);
     //sphere1->setSpecularity(false);
-    sphere1->setReflectivity(true, 0.8);
     sceneObjects.push_back(sphere1);         //Add sphere to scene objects
 
 
-    Sphere *sphere2 = new Sphere(glm::vec3(5.0, -5.0, -40.0), 5.0);
+    Sphere *sphere2 = new Sphere(glm::vec3(5.0, -5.0, -60.0), 5.0);
     sphere2->setColor(glm::vec3(0, 1, 0));
     //sphere2->setShininess(5);
+    sphere2->setTransparency(true, 0.1);
     sceneObjects.push_back(sphere2);         //Add sphere to scene objects
 
-    Sphere *sphere3 = new Sphere(glm::vec3(5.0, 5.0, -70.0), 4.0);
+    Sphere *sphere3 = new Sphere(glm::vec3(-5.0, 0.0, -70.0), 4.0);
     sphere3->setColor(glm::vec3(1, 0, 0));
+    sphere3->setReflectivity(true, 0.8);
     sceneObjects.push_back(sphere3);         //Add sphere to scene objects
 
-    Sphere *sphere4 = new Sphere(glm::vec3(23.0, 10, -130.0), 3.0);
-    sphere4->setColor(glm::vec3(0, 1, 1));
-    sceneObjects.push_back(sphere4);         //Add sphere to scene objects
 
 }
 
